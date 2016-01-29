242f37ffe95bf8d24f9ccb2368f746ada24c4d00
Viet Trung Truong <viettrung9012@yahoo.com>
2016-01-29 22:37:20 +0800

    Initial Commit

---
+++ CP3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CP3
+//
+//  Created by Viet Trung Truong on 29/1/16.
+//  Copyright © 2016 Viet Trung Truong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
